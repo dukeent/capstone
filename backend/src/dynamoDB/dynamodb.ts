ae105bf84c96dc79b7ea8767884f37fc86ab022c
dukeent <122994459+dukentt@users.noreply.github.com>
2023-11-02 10:33:16 +0700

    add xray for dynamodb

--- backend/src/dynamoDB/dynamodb.ts
+++ backend/src/dynamoDB/dynamodb.ts
@@ -1,8 +1,14 @@
 import * as AWS from "aws-sdk";
 import { DocumentClient } from "aws-sdk/clients/dynamodb";
+import * as AWSXRay from "aws-xray-sdk-core";
+
+const XAWS = AWSXRay.captureAWS(AWS);
 
 export const documentClient = (): DocumentClient => {
-  return new AWS.DynamoDB.DocumentClient();
+  //return new AWS.DynamoDB.DocumentClient();
+  return new XAWS.DynamoDB.DocumentClient({
+    signatureVersion: "v4",
+  });
 };
 
 export default documentClient;
